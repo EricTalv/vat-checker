9444a65272e1377eb77942e8d0bd6df4e7ce98c8
EricTalv <eric.talviste@tptlive.ee>
2020-06-08 18:53:21 +0300

    basic logic is set

--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,26 @@
 
         [*] Front-end initial styles V1
 
-        [ ] Send VAT-ID request to endpoint
-        [ ] Retrieve Data 
-        [ ] Display retrieved data
+        [*] Send VAT-ID request to endpoint
+          [*] send a -succesful- request to url
+        [*] Retrieve Data 
+        [*] Display retrieved data
+
+        [*] Prettify JSON
+        [*] Return data Form
+          [ ] Make a good looking display 
+
+        [ ] Implement Loading animation on validate
+
         [ ] Populate Country list
     
     -->
 
   <div class="container">
 
-    <div class="row align-middle h-100">
+    <div class="row align-middle h-50">
 
-      <b-form @submit="onSubmit" @reset="onReset" >
+      <b-form @submit="onSubmit" class="w-50">
 
         <b-form-group
           label="VAT Identifier"
@@ -28,11 +36,12 @@
         >
 
           <!-- VAT ID -->
-          <b-form-group label="VAT code">
+          <b-form-group label="VAT Number">
             <b-form-input
-              id="vat-code-input"
+              id="vat-number-input"
               type="text"         
-              v-model="form.vatCode"
+              v-model="form.VATNumber"
+              size="lg"
             ></b-form-input>
           </b-form-group>
 
@@ -41,52 +50,73 @@
             <b-form-select
               id="vat-country-input"
               v-model="form.country"
-              :options="[{ text: 'Choose...', value: null }, 'One', 'Two', 'Three']"
+              :options="['EE', 'BG', 'RU']"
+              size="lg"
+
             ></b-form-select>
           </b-form-group>
 
           <!-- Submit -->
-          <b-button block squared variant="primary" type="submit">Validate</b-button>
-
+          <b-button block squared size="lg" variant="primary" type="submit">Validate</b-button>
 
         </b-form-group>
 
-
       </b-form>
-
 
     </div>
 
+    <!-- Show this only when data exists -->
+    <div class="row" v-if="items">
+      <b-col>
+        <b-form-group
+          label="Result"
+          class="border p-4 vat-returned-container"
+        >
+
+          <b-table stacked :items="items"></b-table>
+
+        </b-form-group>  
+      </b-col>
+    </div> 
   </div>
 
 </template>
 
 <script>
 
+
   export default {
+
     data() {
       return {
         form: {
-          vatCode: '',
-          country: ''
-        }
-      }
-    },
-
-    methods: {
-        onSubmit(evt) {
-          evt.preventDefault()
-          alert(JSON.stringify(this.form))
+          VATNumber: '100247019',
+          country: 'EE'
         },
 
+        items: null
+      }
+    },
+  
+    methods: {
+        onSubmit(evt) {
 
-        onReset(evt) {
           evt.preventDefault()
-          this.form.vatCode = ''
-          this.form.country = ''
-        }
-    }
 
+          // Using Axios to handle requests
+          this.$axios
+            .get(`https://vat.erply.com/numbers?vatNumber=${this.form.country}${this.form.VATNumber}`) // sends request
+            .then((response) => {
+              // put Response.DATA into Returned Data Object
+              this.items = [response.data]
+              console.log(response.status)
+
+            }) // Handles returned data
+
+          
+        
+        },
+    }
   }
 
 
@@ -101,7 +131,8 @@ html, body {
 
 .container { 
   height: 90vh;
-  font-family: Consolas, sans-serif;
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 20px;
 }
 
 .row {
@@ -111,7 +142,7 @@ html, body {
 
 }
 
-.vat-id-container > legend {
+.vat-id-container > legend, .vat-returned-container > legend {
   width: auto; 
   padding: 0 20px; 
   border-bottom: none;
